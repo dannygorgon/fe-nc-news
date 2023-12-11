cd0f67767214e7d483ec4b16794cb26cf5bf0282
dannygorgon <thirdoptionisthename@googlemail.com>
2023-12-11 10:49:43 +0000

    add initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Button from '@mui/material/Button';
+import React from 'react';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Button variant="contained" color="primary">
+        Click me
+      </Button>
+
+    <h1>I am the App</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import CssBaseline from '@mui/material/CssBaseline';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+       <CssBaseline />
+    <App />
+  </React.StrictMode>,
+)
