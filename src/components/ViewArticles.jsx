13febcdd43fb36e805cfc823de93e1971d215381
dannygorgon <thirdoptionisthename@googlemail.com>
2023-12-11 16:46:58 +0000

    add ViewArticles component

---
+++ src/components/ViewArticles.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ViewArticles() {
+  return (
+    <div>ViewArticles</div>
+  )
+}
+
+export default ViewArticles
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import CssBaseline from '@mui/material/CssBaseline';
+import ViewArticles from './components/ViewArticles.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
        <CssBaseline />
     <App />
+    <ViewArticles />
   </React.StrictMode>,
 )
