72c165f12139f823455568bc77c57b76446449db
dannygorgon <thirdoptionisthename@googlemail.com>
2023-12-12 13:15:03 +0000

    add article page and route, WIP

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Home from './components/pages/Home';
-
-
+import Article from './components/pages/Article';
 function App() {
   return (
     <Router>
 
       <Routes>
         <Route path="/" element={<Home />} /> 
+        <Route path="/articles/:articleId" element={<Article />} />
       </Routes>
     </Router>
   )
---
+++ src/components/pages/Article.jsx
@@ -0,0 +1,9 @@
+const Article = () => {
+    return (
+        <div className="article-card-container">
+            <h1>Jo</h1>
+        </div>
+    )
+}
+
+export default Article
