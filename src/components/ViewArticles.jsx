d02e71bc805b2a7f175d782317c291663400416a
dannygorgon <thirdoptionisthename@googlemail.com>
2023-12-12 10:43:42 +0000

    add date posted and votes

--- src/components/ViewArticles.jsx
+++ src/components/ViewArticles.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+
 import { getAllArticles } from './utils/api';
 function ViewArticles() {
   const [articles, setArticles] = useState([]);
@@ -7,6 +7,7 @@ function ViewArticles() {
   useEffect(() => {
     getAllArticles()
       .then((articlesFromApi) => {
+        console.log(articlesFromApi);
         setArticles(articlesFromApi);
       })
       .catch((err) => console.error(err));
@@ -16,12 +17,14 @@ function ViewArticles() {
     <div>
       <h1>View Articles</h1>
       {articles.map(article => (
-    
+        
         <div key={article.article_id}>
           <img src={article.article_img_url} alt={`A picture of ${article.title}`} />
           <h2>{article.title}</h2>
+          <p></p>
           <p>Written by {article.author}</p>
-          <a href="">coding</a>
+          <p>Date posted {new Date(article.created_at).toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })}</p>          <p>Votes: {article.votes}</p>
+          <a href="">coding</a>e
         </div>
       ))}
     </div>
