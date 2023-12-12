015829cfa9628c20aa5b447435164cb6f659cd97
dannygorgon <thirdoptionisthename@googlemail.com>
2023-12-12 11:08:10 +0000

    add WIP loading spinner, add basic styling for article-container and card

--- src/components/ViewArticles.jsx
+++ src/components/ViewArticles.jsx
@@ -3,33 +3,57 @@ import React, { useState, useEffect } from 'react';
 import { getAllArticles } from './utils/api';
 function ViewArticles() {
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   
   useEffect(() => {
     getAllArticles()
       .then((articlesFromApi) => {
         console.log(articlesFromApi);
         setArticles(articlesFromApi);
+        setIsLoading(false);
       })
       .catch((err) => console.error(err));
   }, []);
 
+  if (isLoading)
   return (
-    <div>
-      <h1>View Articles</h1>
-      {articles.map(article => (
-        
-        <div key={article.article_id}>
-          <img src={article.article_img_url} alt={`A picture of ${article.title}`} />
-          <h2>{article.title}</h2>
-       
-          <p>Written by {article.author}</p>
-
-          <p>Posted on {new Date(article.created_at).toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })}</p>          
-          <p>{article.votes} votes</p>
-          <p>{article.comment_count} comments</p>
-          Category: <a href="">coding</a>
+    <div className="loading-spinner">
+      <div className="loadingio-spinner-spinner-j1bk3azki7">
+        <div className="ldio-vudct9dnim">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
-      ))}
+      </div>
+    </div>
+  );
+  return (
+    <div className='article-list-container'>
+ 
+        <h1>View Articles</h1>
+        {articles.map(article => (
+        
+          <div className="article-card" key={article.article_id}>
+            <img src={article.article_img_url} alt={`A picture of ${article.title}`} />
+            <h2>{article.title}</h2>
+        
+            <p>Written by {article.author}</p>
+            <p>Posted on {new Date(article.created_at).toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })}</p>
+            <p>{article.votes} votes</p>
+            <p>{article.comment_count} comments</p>
+            Category: <a href="">coding</a>
+          </div>
+        ))}
+   
     </div>
   );
 }
