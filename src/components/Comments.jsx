0f3557aa92f2136b13dd0dce84958ebdb39e329d
dannygorgon <thirdoptionisthename@googlemail.com>
2023-12-12 15:31:18 +0000

    add basic styling to comment cards, API endpoint, state for cmments and loading

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,14 +1,46 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
 import LoadSpinner from "./LoadSpinner";
 import { getCommentsByArticleId } from "./utils/api";
 
 
 function Comments() {
-  return (
-    <div>
-        <h1>I WILL BE THE COMMENTS</h1>
-    </div>
-  )
-}
+    const { articleId } = useParams(); // Get the articleId from the URL parameters
+    const [comments, setComments] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+  
+    useEffect(() => {
+      setIsLoading(true);
+      getCommentsByArticleId(articleId)
+        .then((commentsFromApi) => {
+          setComments(commentsFromApi);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          console.error(err);
+          setIsLoading(false);
+        });
+    }, [articleId]);
+  
+    if (isLoading) {
+      return <LoadSpinner />;
+    }
+  
+    return (
+      <div>
+        <h1>Comments</h1>
+        {comments.map((comment) => (
+              <div key={comment.comment_id}>
+                <div className="comments p-4 bg-slate-400 m-4">
 
-export default Comments
+              <p>{comment.body}</p>
+              <p>Author: {comment.author}</p>
+              <p>Posted on: {new Date(comment.created_at).toLocaleDateString()}</p>
+                </div>
+            </div>
+        ))}
+      </div>
+    );
+  }
+  
+  export default Comments;
