5b8bcf9d7b2aee9d27f96ae12850d28e731f1a33
dannygorgon <thirdoptionisthename@googlemail.com>
2023-12-13 14:34:19 +0000

    add WIP log in and user states

--- contexts/UserContexts.jsx
+++ contexts/UserContexts.jsx
@@ -1,9 +1,13 @@
-import { createContext } from "react";
-
+import { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
 export const UserProvider = (props) => {
-    console.log(props);
-    return <UserContext.Provider value={{user: "dannycodes"}}>{props.children}</UserContext.Provider>
-}
+    const [user, setUser] = useState("dannycodes");
+    const [loggedIn, setLoggedIn] = useState(true);
+  return (
+    <UserContext.Provider value={{user, setUser, loggedIn, setLoggedIn}}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,31 @@
 import { useContext } from "react"
 import { UserContext } from "../../contexts/UserContexts"
-
+import Button from '@mui/material/Button';
 const Header = () => {
-    const {user} = useContext(UserContext);
+    const { user, setUser, loggedIn, setLoggedIn } = useContext(UserContext);
+
+    const logout = () => {
+      // Set user to null and loggedIn to false
+      setUser(null);
+      setLoggedIn(false);
+    };
+  
   
     return (
         <header>
+            <div className="m-5 header-container flex justify-between">
             <h1>NC News</h1>
-            <p>Logged in as {user}</p>
-     
+            <div className="flex flex-col items-center justify-center"> 
+                {loggedIn && <p>Logged in as {user}</p>}
+                {loggedIn ? (
+                    <Button variant="contained" onClick={logout}>Logout</Button>
+                ) : (
+                    <Button variant="contained">Login</Button>
+                )}
+            </div>
+            </div>
+           
+
         </header>
     )
 }
