df54a02b12060f325e1a9178b7b1618f2b9335a7
dannygorgon <thirdoptionisthename@googlemail.com>
2023-12-14 15:10:19 +0000

    add WIP delete comment functionality

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import LoadSpinner from "./LoadSpinner";
-import { getCommentsByArticleId, postComment } from "./utils/api";
+import { getCommentsByArticleId, postComment, apiDeleteComment} from "./utils/api";
 import SubmitComment from "./SubmitComment";
 import { UserContext } from "../../contexts/UserContexts";
 
@@ -33,6 +33,16 @@ function Comments() {
       .catch((err) => console.error(err));
   };
 
+  const handleDeleteComment = (comment_id) => {
+    apiDeleteComment(comment_id)
+      .then(() => {
+        setComments((prevComments) => {
+          return prevComments.filter((comment) => comment.comment_id !== comment_id);
+        });
+      })
+      .catch((err) => console.error(err));
+  }
+
   if (isLoading) {
     return <LoadSpinner />;
   }
@@ -41,18 +51,18 @@ function Comments() {
     <div>
       <h1 className="text-2xl">Comments</h1>
       <SubmitComment onSubmit={handleCommentSubmit} />
-      {comments.map((comment) => (
-        <div key={comment.comment_id}>
-          <div className="comments p-4 bg-slate-400 m-4">
-            <p>Author: {comment.author}</p>
-            <p>{comment.body}</p>
-
-            <p>
-              Posted on: {new Date(comment.created_at).toLocaleDateString()}
-            </p>
-          </div>
-        </div>
-      ))}
+      {comments.map((comment) => {
+  console.log(comment);
+  return (
+    <div key={comment.comment_id}>
+      <div className="comments p-4 bg-slate-400 m-4">
+        <p>Author: {comment.author}</p>
+        <p>{comment.body}</p>
+        <button onClick={() => handleDeleteComment(comment.comment_id)}>Delete</button>
+      </div>
+    </div>
+  );
+})}
     </div>
   );
 }
