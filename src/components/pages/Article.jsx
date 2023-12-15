9c0c3d19e48b55646b58a82bf194c52c79c7d7b2
dannygorgon <thirdoptionisthename@googlemail.com>
2023-12-15 10:06:28 +0000

    add formatting

--- src/components/pages/Article.jsx
+++ src/components/pages/Article.jsx
@@ -8,14 +8,13 @@ import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 
-
 const Article = () => {
   const { articleId } = useParams();
   const [article, setArticle] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [votes, setVotes] = useState(0);
   const [error, setError] = useState(null);
-  const [hasVoted , setHasVoted] = useState(false);
+  const [hasVoted, setHasVoted] = useState(false);
 
   useEffect(() => {
     getArticleById(articleId)
@@ -29,7 +28,7 @@ const Article = () => {
 
   const handleIncrementVote = (increment) => {
     if (hasVoted) {
-      setError('You have already voted');
+      setError("You have already voted");
       return;
     }
     setVotes((previousVote) => previousVote + increment);
@@ -56,42 +55,42 @@ const Article = () => {
   return (
     <div className="article-container p-4 m-4  bg-gradient-to-t from-blue-500 to-neutral-300">
       <img
-      className="block mx-auto"
+        className="block mx-auto"
         src={article.article_img_url}
         alt={`A picture of ${article.title}`}
       />
       <h1 className="text-3xl font-bold text-center">{article.title}</h1>
-      <div className="detail-text flex justify-evenly items-center"> 
-  <p>Author: {article.author}</p>
-  <p>
-    Posted: { "  "}
-    {new Date(article.created_at).toLocaleDateString("en-GB", {
-      day: "numeric",
-      month: "long",
-      year: "numeric",
-    })}
-  </p>
+      <div className="detail-text flex justify-evenly items-center">
+        <p>Author: {article.author}</p>
+        <p>
+          Posted: {"  "}
+          {new Date(article.created_at).toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          })}
+        </p>
 
-  {error && <p>{error}</p>}
-  <div className="flex items-center"> 
-    <Stack direction="row" spacing={1}>
-      <Chip
-        icon={<ThumbUpIcon />}
-        label={`Votes: ${votes}`}
-        clickable
-        onClick={() => handleIncrementVote(1)}
-      />
-    </Stack>
-  </div>
-</div>
+        {error && <p>{error}</p>}
+        <div className="flex items-center">
+          <Stack direction="row" spacing={1}>
+            <Chip
+              icon={<ThumbUpIcon />}
+              label={`Votes: ${votes}`}
+              clickable
+              onClick={() => handleIncrementVote(1)}
+            />
+          </Stack>
+        </div>
+      </div>
 
       <div className="article-body">
         <p>{article.body}</p>
       </div>
 
       <p>
-          Category: <a href="">coding</a>
-        </p>
+        Category: <a href="">coding</a>
+      </p>
       <div className="comment-container">
         <Comments articleId={articleId} />
       </div>
